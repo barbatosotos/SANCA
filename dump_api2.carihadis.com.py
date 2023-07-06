1041c548374117bcc2dddc65ebcea56e1fef7728
sugeng barokatul anam <56747970+barbatosotos@users.noreply.github.com>
2023-07-07 05:01:23 +0700

    Update dump_api2.carihadis.com.py

--- dump_api2.carihadis.com.py
+++ dump_api2.carihadis.com.py
@@ -1,8 +1,9 @@
 ## dump api2.carihadis.com to csv
-
+## output CSV separator Tab
 import csv
 import requests
 import time
+
 # Fungsi untuk mendapatkan data hadis dari URL
 def get_hadith_data(url):
     response = requests.get(url)
@@ -37,28 +38,30 @@ for i, kitab in enumerate(kitab_list, start=1):
     print(f'{i}. {kitab}')
 kitab_index = int(input("Kitab nomor berapa? (Jawaban dalam angka): ")) - 1
 kitab = kitab_list[kitab_index]
-berapa_data = int(input("Berapa data yang ingin diambil? (Jawaban dalam angka): "))
+dari = input("Dari hadits ke berapa? (Jawaban dalam angka): ")
+sampai = input("Sampai hadits ke berapa? (Jawaban dalam angka): ")
+delay = float(input("Delay waktu tiap eksekusi (dalam detik): "))
 
 # Membuat URL berdasarkan input pengguna
 url = f"https://api2.carihadis.com/?kitab={kitab}&id={{hadith_id}}"
 
 # Format nama file
-nama_file = f"{berapa_data}_data_{kitab}.csv"
+nama_file = f"{dari}_{sampai}_{kitab}.csv"
 
 # Loop melalui rentang hadis
 with open(nama_file, 'w', encoding='utf-8', newline='') as file:
     writer = csv.writer(file, delimiter='\t')
     writer.writerow(['Hadith ID', 'Nass', 'Terjemah'])
 
-    if berapa_data == 1:
-        nomor = input("Masukkan nomor hadith yang ingin diambil: ")
-        current_url = url.format(hadith_id=nomor)
-        try:
-            hadith_data = get_hadith_data(current_url)
-            hadith_id = list(hadith_data.keys())[0]
-            hadith_nass = hadith_data[hadith_id]['nass']
-            hadith_terjemah = hadith_data[hadith_id]['terjemah']
-            writer.writerow([hadith_id, hadith_nass, hadith_terjemah])
-            print(f'Data hadis ID {hadith_id} dari URL {current_url} ditambahkan ke CSV')
-        except Exception as e:
-            print(f'Gagal mengambil data hadis dari URL {current_url}: {str(e)}')
+    for hadith_id in range(int(dari), int(sampai) + 1):
+        current_url = url.format(hadith_id=hadith_id)
+        hadith_data = get_hadith_data(current_url)
+        hadith_nass = hadith_data['nass']
+        hadith_terjemah = hadith_data['terjemah']
+        writer.writerow([hadith_id, hadith_nass, hadith_terjemah])
+        print(f'Data hadis ID {hadith_id} dari URL {current_url} ditambahkan ke CSV')
+
+        # Delay sebelum eksekusi berikutnya
+        time.sleep(delay)
+
+print('Proses penyimpanan data ke CSV selesai.')
